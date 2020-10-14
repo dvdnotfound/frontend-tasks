7b8b4ea50d3914b2e23a163c40b293a74a2637b5
David Santos <contato.dvdsantos@gmail.com>
2020-10-14 19:28:11 -0300

    State-and-Effect

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -63,7 +63,7 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a href="https://www.google.com/maps">Ver rotas no Google Maps</a>
               </footer>
             </div>
 
